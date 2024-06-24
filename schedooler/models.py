318086193609d9c233040f47f33c1c0961cc9ed3
jhlee <jhlee9462@github.com>
2024-06-24 20:35:03 +0900

    feat. define schedool model

--- schedooler/models.py
+++ schedooler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Schedool(models.Model):
+    datetime = models.DateTimeField(null=False)
+    name = models.CharField(max_length=100, null=False)
+    content = models.TextField(max_length=500)
+    done = models.BooleanField(default=False, null=False)
