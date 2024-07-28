71060a1ad088af8893d88716e793a91ff1fc9c9e
jhlee <jhlee9462@gmail.com>
2024-07-28 15:24:53 +0900

    fix. 모델 설정 오류 수정

--- schedooler/models.py
+++ schedooler/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Schedool(models.Model):
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     datetime = models.DateTimeField()
     title = models.CharField(max_length=100, null=False)
     content = models.TextField(max_length=500)
@@ -11,9 +11,9 @@ class Schedool(models.Model):
 
 
 class Routine(models.Model):
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     schedool = models.OneToOneField(Schedool, on_delete=models.DO_NOTHING)
-    routine_rule = models.CharField(null=False)
+    routine_rule = models.CharField(max_length=255, null=False)
     start_time = models.DateTimeField(null=False)
     end_time = models.DateTimeField()
 
